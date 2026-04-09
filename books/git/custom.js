b8fff7c076bbfbb22079e8c1e5c76f0c31dd351c
Gabor Szabo <gabor@szabgab.com>
2026-04-09 13:23:42 +0200

    filanames

---
+++ books/git/custom.js
@@ -0,0 +1,4 @@
+const prefixes = ["src/examples/"];
+const extensions = [".pl", ".pm", ".t", ".toml", ".yaml", ".cfg", ".html", ".PL", ".rs", ".py"];
+const base_url = "https://github.com/szabgab/git.code-maven.com/tree/main/books/git/"
+
